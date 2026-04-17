cd5778eeec0a1ce620659fecee659bf2e4bfdd81
Tamil <tstamilvs@gmail.com>
2026-04-17 15:44:14 +0530

    pages created

--- SeleniumUIFramework.RedbusApp/src/main/java/com/redbus/testing/utilities/Pages.java
+++ SeleniumUIFramework.RedbusApp/src/main/java/com/redbus/testing/utilities/Pages.java
@@ -1,5 +1,20 @@
 package com.redbus.testing.utilities;
 
-public class Pages {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
+import com.redbus.testing.pages.DashBoardPage;
+import com.redbus.testing.pages.HotelSearchResultsPage;
+import com.redbus.testing.pages.HotelsPage;
+
+public class Pages {
+	public static DashBoardPage dp;
+	public static HotelSearchResultsPage hsrp ;
+	public static HotelsPage hp;
+	
+	public static void loadAllPages(WebDriver driver) {
+		dp=PageFactory.initElements(driver, DashBoardPage.class);
+		hsrp=PageFactory.initElements(driver, HotelSearchResultsPage.class);
+		hp=PageFactory.initElements(driver, HotelsPage.class);
+	}
 }
